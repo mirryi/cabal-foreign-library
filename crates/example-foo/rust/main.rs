3c90d3a79b59c3cb34992b394bba7c9948c8fa14
Eric Zhao <21zhaoe@protonmail.com>
2024-11-08 15:03:44 -0500

    add binary to example-foo

---
+++ crates/example-foo/rust/main.rs
@@ -0,0 +1,11 @@
+use std::ptr::null_mut;
+
+use example_foo::{foo, hs_exit, hs_init};
+
+fn main() {
+    unsafe {
+        hs_init(null_mut(), null_mut());
+        println!("Haskell returns: {}", foo(42));
+        hs_exit();
+    }
+}
