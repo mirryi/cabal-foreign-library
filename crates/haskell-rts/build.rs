1cf2ab2a7063c8e5e87c51bb50172216a76e0af1
Eric Zhao <21zhaoe@protonmail.com>
2024-11-08 16:02:21 -0500

    start higher level wrapper for HSrts

---
+++ crates/haskell-rts/build.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::path::PathBuf;
+
+use cabal_foreign_library::Build;
+
+fn main() {
+    let mut cabal = Build::new().unwrap();
+
+    // build cabal project
+    let lib = cabal.build().expect("failed to build cabal project");
+
+    // generate and write bindings
+    let bindings_file = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+    lib.bindings()
+        .expect("failed to generate bindings")
+        .write_to_file(bindings_file)
+        .expect("failed to write bindings");
+
+    // link the library, modifying rpath
+    lib.link(true).expect("failed to link cabal library");
+
+    // link the system dependencies, modifying rpath
+    lib.link_system(true)
+        .expect("failed to link Haskell system libraries");
+}
